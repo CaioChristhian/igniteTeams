ee1a76f51c26bafe5e921a45a4d4c765d7e3af5f
Caio Christhian Lopes Silva <caio.christhian@uft.edu.br>
2023-09-29 13:39:59 -0300

    first commit

---
+++ src/components/ListEmpty/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import * as S from './styles';
+
+type Props = {
+  message: string;
+}
+
+export function ListEmpty({ message }: Props){
+  return (
+    <S.Container>
+      <S.Message>
+        {message}
+      </S.Message>
+    </S.Container>
+  );
+}
